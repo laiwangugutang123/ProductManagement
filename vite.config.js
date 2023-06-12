969369b0a8c229de1d87d621900c8b7e844ead82
laiwangugutang123 <1436026983@qq.com>
2023-06-13 00:06:48 +0800

    test 1

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,12 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/laiwangugutang123/",
+  build: {
+    outDir: "docs"
+  },
+
+
   plugins: [
     vue(), 
     vueJsx(),
